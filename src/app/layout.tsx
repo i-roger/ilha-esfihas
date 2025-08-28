2e4412fdefbbae4437bcf840c9165ef354eedb3e
i-roger <mroger.dev@gmail.com>
2025-08-28 12:01:43 -0300

    Fix: Tab name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ilha Lanches",
+  title: "Ilha Esfihas",
   description: "As melhores esfihas da ilha.",
 };
 
